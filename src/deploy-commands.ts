c4a1f646764f5e39a188965efad3e5790727499f
Lucas <lucasnunes-tb@hotmail.com>
2022-03-08 12:05:06 -0300

    fix configs

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -12,7 +12,7 @@ const commands = [];
 // for (const module of Object.values<Command>(commandModules)) {
 //   commands.push(module.data);
 // }
-const rest = new REST({ version: '9' }).setToken(config.DISCORD_BOT_TOKEN);
+const rest = new REST({ version: '9' }).setToken(config.BOT_TOKEN);
 
 // rest
 //   .put(Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID), {
